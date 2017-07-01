e2cff708a1bd73743166e68a4f5b3419c162edd0
Yi <yipersevere@outlook.com>
2017-07-01 12:50:35 +0300

    modify

--- hotelreviews/hotelreviews/items.py
+++ hotelreviews/hotelreviews/items.py
@@ -12,3 +12,18 @@ class HotelreviewsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookingReviewItem(scrapy.Item):
+    title = scrapy.Field()
+    score = scrapy.Field()
+    positive_content = scrapy.Field()
+    negative_content = scrapy.Field()
+    tags = scrapy.Field()
+
+class BookingHotelItem(scrapy.Item):
+    name = scrapy.Field()
+    url = scrapy.Field()
+    location = scrapy.Field()
+    score = scrapy.Field()
+    reviews = scrapy.Field()
+
